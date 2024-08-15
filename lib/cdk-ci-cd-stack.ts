293ea631ca7e85adc3e7cb5f5aca07fd62c914ba
joshjewe <joshua-weiske@web.de>
2024-08-15 19:50:48 +0200

    Init

--- lib/cdk-ci-cd-stack.ts
+++ lib/cdk-ci-cd-stack.ts
@@ -14,7 +14,13 @@ export class CdkCiCdStack extends cdk.Stack {
     const pipeline = new CodePipeline(this, "AwesomePipeline", {
       pipelineName: "AwesomePipeline",
       synth: new ShellStep("Synth", {
-        input: CodePipelineSource.gitHub("joshjewe/cdk-ci-cd", "cicd-practice"),
+        input: CodePipelineSource.gitHub(
+          "joshjewe/cdk-ci-cd",
+          "cicd-practice",
+          {
+            authentication: cdk.SecretValue.secretsManager("github-token-v3"),
+          }
+        ),
         commands: ["npm ci", "npm run build", "npx cdk synth"],
       }),
     });
